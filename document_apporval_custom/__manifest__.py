0765a1a1a9f60055d7db46183eb90869a3f59261
ltvu02fdi <ltvu02.fdi@gmail.com>
2025-08-19 09:09:35 +0700

    [ADD] man hinh phe duyet

---
+++ document_apporval_custom/__manifest__.py
@@ -0,0 +1,16 @@
+# -*- coding: utf-8 -*-
+{
+    'name': "Document Approval Custom",
+    "version": "0.1",
+    "category": "Documents Management",
+    "summary": "Manager can approve or reject documents",
+    "description": "User can create and upload various document for approvals."
+                   "Manager can approve or reject documents.",
+    'author': 'Cybrosys Techno Solutions',
+    'depends': ['document_approval', 'hr'],
+    'data': [
+        'security/ir.model.access.csv',
+        'data/data.xml',
+        'views/document_approval_views.xml',
+    ],
+}
---
+++ document_apporval_custom/models/__init__.py
@@ -0,0 +1,3 @@
+# -*- coding: utf-8 -*-
+from . import document_approval
+from . import document_approval_line
